cd8ca67035e5e0f95dcfb83d2115db57823a0967
Beka Sturua <beqasturua3@gmail.com>
2022-07-04 10:26:03 +0400

    added links

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,15 @@
+import { Link } from "react-router-dom";
+
 export default function Home() {
-  return <h1>Home Page</h1>;
+  return (
+    <div>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/product">Product</Link>
+      </nav>
+      <h1>Home Page</h1>
+    </div>
+  );
 }
+
+
