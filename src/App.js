d297f4965ac9ae761f1d0762a4e55d73e8885f6b
Beka Sturua <beqasturua3@gmail.com>
2022-07-05 13:41:21 +0400

    add :productId

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/product" element={<Product />} />
+          <Route path="/product/:productId" element={<Product />} />
         </Routes>
       </BrowserRouter>
     </QueryClientProvider>
