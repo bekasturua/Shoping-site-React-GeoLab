d72cbe7a005a2e3978c6d980e9407aae778cabd8
Beka Sturua <beqasturua3@gmail.com>
2022-07-05 13:41:48 +0400

    added imte.id links

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import Page from "./pages";
 import { useQuery } from "react-query";
 import apiRequest from "../apiRequest";
+import { Link } from "react-router-dom";
 
 export default function Home() {
   const { data } = useQuery("products", () =>
@@ -15,9 +16,13 @@ export default function Home() {
         {(data || []).map((item) => (
           <div className="col" key={item.id}>
             <div className="card h-100">
-              <img src={item.image} className="card-img-top" alt="..." />
+              <Link to={`/product/${item.id}`}>
+                <img src={item.image} className="card-img-top" alt="..." />
+              </Link>
               <div className="card-body">
-                <h5 className="card-title">{item.title}</h5>
+                <Link to={`/product/${item.id}`}>
+                  <h5 className="card-title">{item.title}</h5>
+                </Link>
                 <p className="card-text">${item.price}</p>
                 <button>Add to cart</button>
               </div>
